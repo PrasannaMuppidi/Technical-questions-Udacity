75f56319ee55b64e7522348eac840f809f414cb6
Prasanna Muppidi <pmyrd@mail.umkc.edu>
2017-06-22 23:51:00 -0500

    feat: Update 4.py

--- 4.py
+++ 4.py
@@ -19,89 +19,27 @@ question4([[0, 1, 0, 0, 0],
           1,
           4)
 """
-head = None
-
-class Node(object):
-  def __init__(self, data):
-    self.data = data
-    self.right = None
-    self.left = None
-
-# Function to insert a new node at the beginning
-def push_right(node, new_data):
-    new_node = Node(new_data)
-    node.right = new_node
-    return new_node
-
-# Function to insert a new node at the beginning
-def push_left(node, new_data):
-    new_node = Node(new_data)
-    node.left = new_node
-    return new_node
-
-# Function to find LCA of n1 and n2. The function assumes
-# that both n1 and n2 are present in BST
-def lca(head, n1, n2):
-    # Base Case
-    if head is None:
-        return None
-
-    # If both n1 and n2 are smaller than root, then LCA
-    # lies in left
-    if(head.data > n1 and head.data > n2):
-        return lca(head.left, n1, n2)
-
-    # If both n1 and n2 are greater than root, then LCA
-    # lies in right
-    if(head.data < n1 and head.data < n2):
-        return lca(head.right, n1, n2)
-
-    return head.data
+def question4(T, r, n1, n2):
+    n1_ps = []
+    while n1 != r:
+        n1 = parent(T, n1)
+        n1_ps.append(n1)
+    if len(n1_ps) == 0:
+        return -1
+    while n2 != r:
+        n2 = parent(T, n2)
+        if n2 in n1_ps:
+            return n2
+    return -1
+    
+    
+def parent(T, n):
+	#return parent of n if it exists, otherwise return -1
+    numrows = len(T)
+    for i in range(numrows):
+        if T[i][n] == 1:
+            return i
+    return -1
 
 
-def question4(mat, root, n1, n2):
-    global head
-    # Make BST
-    head = Node(root)
-    head.left, head.right = None, None
-    node_value = 0
-    tmp_right, tmp_left = None, None
-    node_list = []
-    for elem in mat[root]:
-        if elem:
-            if(node_value>root):
-                node_list.append(push_right(head, node_value))
-            else:
-                node_list.append(push_left(head, node_value))
-        node_value += 1
-
-    tmp_node = node_list.pop(0)
-    while tmp_node != None:
-        node_value = 0
-        for elem in mat[tmp_node.data]:
-            if elem:
-                if(node_value>tmp_node.data):
-                    node_list.append(push_right(tmp_node, node_value))
-                else:
-                    node_list.append(push_left(tmp_node, node_value))
-            node_value += 1
-        if node_list == []:
-            break
-        else:
-            tmp_node = node_list.pop(0)
-
-    return lca(head, n1, n2)
-
-# Main program
-def main():
-    global head
-
-    print question4([[0, 0, 0, 0, 0],
-                     [1, 0, 1, 0, 0],
-                     [0, 0, 0, 0, 0],
-                     [0, 1, 0, 0, 1],
-                     [0, 0, 0, 0, 0]],
-                     3, 1, 2)
-
-if __name__ == '__main__':
-    main()
+print question4([[0,1,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,1],[0,0,0,0,0]],3,1,4)
